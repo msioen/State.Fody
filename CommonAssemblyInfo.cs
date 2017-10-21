369f0224b901330ae514592563466ee9a9d9888c
Michiel Sioen <michiel.sioen@flowpilots.com>
2017-10-21 15:46:28 +0200

    initial commit

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("State.Fody")]
+[assembly: AssemblyProduct("State.Fody")]
+[assembly: AssemblyCompany("Michiel Sioen")]
+[assembly: AssemblyDescription("Fody add-in for tracking state during method execution.")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
---
+++ State.Fody/ModuleWeaver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Mono.Cecil;
+
+namespace State.Fody
+{
+    public class ModuleWeaver
+    {
+        public Action<string> LogWarning { get; set; }
+        public Action<string> LogInfo { get; set; }
+        public Action<string> LogDebug { get; set; }
+
+        public ModuleDefinition ModuleDefinition { get; set; }
+
+        public ModuleWeaver()
+        {
+            LogWarning = s => { };
+            LogInfo = s => { };
+            LogDebug = s => { };
+        }
+
+        public void Execute()
+        {
+            // weaving code
+        }
+    }
+}
